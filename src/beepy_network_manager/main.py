81eef6087aff7a9923adbcdc67248112346b7d14
Conor Flynn <conor.j.flynn@gmail.com>
2024-11-02 16:39:04 +0100

    Fixes CLI (1.1.0)

--- src/beepy_network_manager/main.py
+++ src/beepy_network_manager/main.py
@@ -1,15 +1,21 @@
+import asyncio
+import sys
+
 from .cli import main as cli_main
 from .tui import BeepyNetworkManagerApp
 
 
-def main() -> None:
-    import sys
-
+async def main() -> None:
     if len(sys.argv) > 1:
-        cli_main()
+        await cli_main()
     else:
-        BeepyNetworkManagerApp().run()
+        app = BeepyNetworkManagerApp()
+        await app.run_async()
+
+
+def run_main():
+    asyncio.run(main())
 
 
 if __name__ == "__main__":
-    main()
+    run_main()
